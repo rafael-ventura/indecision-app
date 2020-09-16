68be983bb18824e462f728d86cb31d7618d76a83
Rafael Ventura <raf4ael@gmail.com>
2020-09-15 23:41:07 -0300

    preparando babel-loader no webpack e organizando o projeto

--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 20));
-
-import isSenior, { isAdult, canDrink } from './person.js';
-console.log(isAdult(18));
-console.log(canDrink(20));
-console.log(isSenior(65));
+const template = <p>THIS IS JSX FROM WEBPACK</p>;
+ReactDOM.render(template, document.getElementById('app'));
